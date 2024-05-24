615c4461461eaff02f6949e49284e15c1c21a558
Said <alhendisaid@gmail.com>
2024-05-24 16:10:37 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <h3 className="text-7xl font-bold">APP</h3>
+      <Button>WAS</Button>
+    </div>
+  )
+}
+export default App 
